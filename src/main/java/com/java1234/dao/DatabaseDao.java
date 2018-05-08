fca3e4fde846b7a3700f855cbc7ed3821041234c
zhang0135789 <zz634682577>
2018-05-08 17:12:21 +0800

    添加database实体 2018年5月8日17:12:15 by zz

---
+++ src/main/java/com/java1234/dao/DatabaseDao.java
@@ -0,0 +1,84 @@
+package com.java1234.dao;
+
+import com.java1234.entity.Database;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @Author: zz
+ * @Description: 采集数据信息Dao接口
+ * @Date: 下午 4:53 2018/5/8 0008
+ * @Modified By
+ */
+public interface DatabaseDao {
+
+    /**
+     * 根据日期月份分组查询
+     * @return
+     */
+//    public List<Database> countList();
+
+    /**
+     * 分页查询
+     * @return
+     */
+    public List<Database> list(Map<String,Object> map);
+
+    /**
+     * 获取总记录数
+     * @param map
+     * @return
+     */
+    public Long getTotal(Map<String,Object> map);
+
+    /**
+     * 通过Id查找实体
+     * @param id
+     * @return
+     */
+    public Database findById(Integer id);
+
+    /**
+     * 更新信息
+     * @param data
+     * @return
+     */
+    public Integer update(Database data);
+
+    /**
+     * 获取上一个
+     * @param id
+     * @return
+     */
+//    public Database getLastDatabase(Integer id);
+
+    /**
+     * 获取下一个
+     * @param id
+     * @return
+     */
+//    public Database getNextDatabase(Integer id);
+
+    /**
+     * 添加信息
+     * @param blog
+     * @return
+     */
+ //   public Integer add(Database blog);
+
+    /**
+     * 删除信息
+     * @param id
+     * @return
+     */
+    public Integer delete(Integer id);
+
+    /**
+     * 查询指定的博客类别下的博客数量
+     * @param typeId
+     * @return
+     */
+ //   public Integer getDatabaseByTypeId(Integer typeId);
+
+}
---
+++ src/main/java/com/java1234/entity/Database.java
@@ -0,0 +1,105 @@
+package com.java1234.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * @Author: zz
+ * @Description: 采集数据信息实体
+ * @Date: 下午 4:22 2018/5/8 0008
+ * @Modified By
+ */
+public class Database  {
+
+    private Integer id; // 编号
+    private String dataId; //数据随机号，主键
+    private String dataName; // 数据名字
+    private String filePath; // 数据文件路径
+    private Date upDate; // 采集数据时间
+    private Integer stateAnalyze; // 分析状态
+    private Date anDate; // 分析时间
+    private Integer stateForecast; // 预测状态
+    private Date foDate; //预测时间
+    private String downloadOPath; //下载路径
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDataId() {
+        return dataId;
+    }
+
+    public void setDataId(String dataId) {
+        this.dataId = dataId;
+    }
+
+    public String getDataName() {
+        return dataName;
+    }
+
+    public void setDataName(String dataName) {
+        this.dataName = dataName;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public Date getUpDate() {
+        return upDate;
+    }
+
+    public void setUpDate(Date upDate) {
+        this.upDate = upDate;
+    }
+
+    public Integer getStateAnalyze() {
+        return stateAnalyze;
+    }
+
+    public void setStateAnalyze(Integer stateAnalyze) {
+        this.stateAnalyze = stateAnalyze;
+    }
+
+    public Date getAnDate() {
+        return anDate;
+    }
+
+    public void setAnDate(Date anDate) {
+        this.anDate = anDate;
+    }
+
+    public Integer getStateForecast() {
+        return stateForecast;
+    }
+
+    public void setStateForecast(Integer stateForecast) {
+        this.stateForecast = stateForecast;
+    }
+
+    public Date getFoDate() {
+        return foDate;
+    }
+
+    public void setFoDate(Date foDate) {
+        this.foDate = foDate;
+    }
+
+    public String getDownloadOPath() {
+        return downloadOPath;
+    }
+
+    public void setDownloadOPath(String downloadOPath) {
+        this.downloadOPath = downloadOPath;
+    }
+}
