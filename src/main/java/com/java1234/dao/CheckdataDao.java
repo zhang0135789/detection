8e9b76fe083170e41528acdb1a55426d932fb0dc
zhang0135789 <zz634682577@163.com>
2018-05-13 10:26:18 +0800

    添加dao层增删改查操作 2018年5月13日09:51:22 by zz

--- src/main/java/com/java1234/dao/CheckdataDao.java
+++ src/main/java/com/java1234/dao/CheckdataDao.java
@@ -19,29 +19,35 @@ public interface CheckdataDao {
      * 分页查询
      * @return
      */
-    public List<Checkdata> list(Map<String,Object> map);
+//    public List<Checkdata> list(Map<String,Object> map);
 
     /**
      * 获取总记录数
      * @param map
      * @return
      */
-    public Long getTotal(Map<String,Object> map);
+//    public Long getTotal(Map<String,Object> map);
 
     /**
      * 通过Id查找实体
-     * @param id
-     * @return
+     * @param dataId
+     * @return List
      */
-    public Checkdata findById(Integer id);
+    public List<Checkdata> findById(String dataId);
 
     /**
      * 更新信息
      * @param data
      * @return
      */
-    public Integer update(Checkdata data);
+//    public Integer update(Checkdata data);
 
+    /**
+     * 添加信息
+     * @param data
+     * @return
+     */
+    public Integer add(Checkdata data);
 
     /**
      * 根据日期月份分组查询
@@ -63,12 +69,7 @@ public interface CheckdataDao {
      */
 //    public Checkdata getNextCheckdata(Integer id);
 
-    /**
-     * 添加信息
-     * @param blog
-     * @return
-     */
-    //   public Integer add(Checkdata blog);
+
 
     /**
      * 删除信息
